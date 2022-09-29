cdc285fe8c17f2683a92a1206b79c4e14b04457f
VilchezMax <maximiliano.vilchez@outlook.com>
2022-09-29 05:16:30 -0300

    Fixed marshalling  date formatting for JAXB

--- Java/vilchezmax/src/main/java/db/DateAdapter.java
+++ Java/vilchezmax/src/main/java/db/DateAdapter.java
@@ -5,15 +5,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateAdapter extends XmlAdapter<String, Date> {
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
     /* Unmarshal: XML to Object */
     @Override
     public Date unmarshal(String dateStr) throws Exception {
-        return new SimpleDateFormat("yyyy-MM-dd").parse(dateStr);
+        return dateFormat.parse(dateStr);
     }
 
     /* Marshal: Object to XML */
     @Override
     public String marshal(Date date) {
-        return date.toString();
+        return dateFormat.format(date);
     }
 }
